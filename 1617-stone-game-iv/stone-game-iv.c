1b4fbadf3d94fcaee7570c3b50bc2810baf59ea6
Anant Acharya <anantacharya5568@gmail.com>
2026-08-10 10:35:15 +0530

    Time: 19 ms (72.73%) | Memory: 8.6 MB (54.55%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.c
@@ -0,0 +1,22 @@
+bool winnerSquareGame(int n)
+{
+    bool dp[n + 1];
+
+    dp[0] = false;
+
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = false;
+
+        for (int j = 1; j * j <= i; j++)
+        {
+            if (!dp[i - j * j])
+            {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[n];
+}
