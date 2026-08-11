f176d4eb6f25fba735df0e25c1c443aaac9ae613
Anant Acharya <anantacharya5568@gmail.com>
2026-08-11 21:06:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (66.67%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.c
@@ -0,0 +1,26 @@
+int missingInteger(int* nums, int numsSize) {
+    int sum = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] == nums[i - 1] + 1)
+            sum += nums[i];
+        else
+            break;
+    }
+
+    while (1) {
+        int found = 0;
+
+        for (int i = 0; i < numsSize; i++) {
+            if (nums[i] == sum) {
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found)
+            return sum;
+
+        sum++;
+    }
+}
