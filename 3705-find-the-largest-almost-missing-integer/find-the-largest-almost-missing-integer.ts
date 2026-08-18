0449b79c3e1c6a607c1d0c66aeca9159f3289fad
Anant Acharya <anantacharya5568@gmail.com>
2026-08-18 17:36:45 +0530

    Time: 1 ms (88.24%) | Memory: 58 MB (58.82%) - LeetSync

---
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.ts
@@ -0,0 +1,36 @@
+function largestInteger(nums: number[], k: number): number
+{
+    const cnt: number[] = Array(51).fill(0);
+
+    for (const x of nums)
+    {
+        cnt[x]++;
+    }
+
+    if (k === nums.length)
+    {
+        return Math.max(...nums);
+    }
+
+    if (k === 1)
+    {
+        let ans = -1;
+
+        for (const x of nums)
+        {
+            if (cnt[x] === 1)
+            {
+                ans = Math.max(ans, x);
+            }
+        }
+
+        return ans;
+    }
+
+    const a = cnt[nums[0]] === 1 ? nums[0] : -1;
+    const b = cnt[nums[nums.length - 1]] === 1
+        ? nums[nums.length - 1]
+        : -1;
+
+    return Math.max(a, b);
+}
